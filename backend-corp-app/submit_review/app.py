ac84d7f1fabfeab21d85883638ae9cf9d3731a5b
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-23 15:59:36 +0300

    testing locally

--- backend-corp-app/submit_review/app.py
+++ backend-corp-app/submit_review/app.py
@@ -2,25 +2,56 @@ import json
 import boto3
 import uuid
 from datetime import datetime
+import os
+
+
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb-local:8000')
+table_name = os.environ.get('DYNAMODB_TABLE')
+table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     
+    try:
+        request_body = json.loads(event["body"])
+    except json.JSONDecodeError:
+        return {
+            'status_code': 400,
+            'body': json.dumps({"errorMessage": "Invalid JSON in request body"})
+        }
+    
     reviewId = str(uuid.uuid4())
-    company = event["body"]["Company"]
-    review = event["body"]["Review"]
-    rating = event["body"]["Rating"]
+    company = request_body["Company"]
+    review = request_body["Review"]
+    rating = request_body["Rating"]
     created_at = str(datetime.now())
     
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('DYNAMODB_TABLE') # type: ignore
-    Item = {
+    
+    new_item = {
         "reviewId": reviewId,
         "company": company,
         "review": review,
         "rating": rating,
         "created_at": created_at
     }
-    table.put_item(Item)
+    
+    try:
+        table.put_item(Item=new_item)
+        
+        response_message = f"Review for {company} submitted successfully!"
+        return {
+            'status_code': 201,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({"message":response_message})
+        }
+    except Exception as e:
+        print(f"Error putting item into DynamoDB: {e}")
+        return {
+            'status_code': 500,
+            'body': json.dumps({"errorMessage": str(e)})
+        }
+    
     
     return ({
         "status_code": 200,
