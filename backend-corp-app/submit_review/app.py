698210adc9adf2159891389d4656bddc679a23ad
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-23 14:24:22 +0300

    review api route

--- backend-corp-app/submit_review/app.py
+++ backend-corp-app/submit_review/app.py
@@ -1,10 +1,26 @@
 import json
 import boto3
+import uuid
+from datetime import datetime
 
 def lambda_handler(event, context):
+    
+    reviewId = str(uuid.uuid4())
+    company = event["body"]["Company"]
+    review = event["body"]["Review"]
+    rating = event["body"]["Rating"]
+    created_at = str(datetime.now())
+    
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('DYNAMODB_TABLE')
-    table.put_item(event)
+    Item = {
+        "reviewId": reviewId,
+        "company": company,
+        "review": review,
+        "rating": rating,
+        "created_at": created_at
+    }
+    table.put_item(Item)
     
     return ({
         "status_code": 200,
