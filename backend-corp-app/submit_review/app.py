e8201341f8afd1735ab695b1b3e47e43dd3f1fb7
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-24 15:16:15 +0300

    updating tables

--- backend-corp-app/submit_review/app.py
+++ backend-corp-app/submit_review/app.py
@@ -7,7 +7,7 @@ import os
 
 dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb-local:8000')
 table_name = os.environ.get('DYNAMODB_TABLE')
-table = dynamodb.Table(table_name)
+table = dynamodb.Table(table_name) # type: ignore
 
 def lambda_handler(event, context):
     
