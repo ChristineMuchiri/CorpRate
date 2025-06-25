b18bd0bf5a2b0c2c153155b3c5297ddee42d865e
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-25 13:21:57 +0300

    event path parameters

--- backend-corp-app/get_reviews/app.py
+++ backend-corp-app/get_reviews/app.py
@@ -2,16 +2,47 @@ import json
 import boto3
 import os
 
-dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb-local:8000')
+#dynamodb = boto3.resource('dynamodb', endpoint_url='http://dynamodb-local:8000')
 table_name = os.environ.get('DYNAMODB_TABLE')
-table = dynamodb.Table(table_name) # type: ignore
+#table = dynamodb.Table(table_name) # type: ignore
+client = boto3.client('dynamodb', endpoint_url='http://dynamodb-local:8000')
 
 def lambda_handler(event, context):
     try:
-        request_body = json.loads(event["body"])
-    except json.JSONDecodeError:
+        company_name = event.get('pathParameters', {}.get('company'))
+        
+        if not company_name:
+            return {
+                'status_code': 400,
+                'body': json.dumps({'errorMessage': "Comapny name not provided in the path."})
+            }
+    except Exception as e:
+        print(f"error extracting namme from path: {e}")
         return {
             'status_code': 400,
-            'body': json.dumps({"errorMessage": "Invalid JSON in request body"})
+            'body': json.dumps({"errorMessage": f"Bad request format: {e}"})
         }
-    
+        
+    try:
+        response = client.query(
+            TableName=table_name,
+            KeyConditionExpression='PK = :pkval',
+            ExpressionAttributeValues={
+                ':pkval': {'S': f"COMPANY#{company_name}"}
+            }, 
+            Select='ALL_ATTRIBUTES'
+        )
+        print(f"Query response: {json.dumps(response, indent=2)}")
+        items = response.get('Items', [])
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps(items)
+        }
+    except Exception as e:
+        print(f"error querying item: {e}")
+        return {
+            "status_code": 500,
+            "body": json.dumps({"errorMessage": str(e)})
+        }
+        
