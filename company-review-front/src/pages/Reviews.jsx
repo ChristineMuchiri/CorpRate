68f95c0e7893bd0d845defb64d4f1badc1d314ee
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-07-09 16:31:23 +0300

    latest reviews

--- company-review-front/src/pages/Reviews.jsx
+++ company-review-front/src/pages/Reviews.jsx
@@ -1,9 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Reviews.css'
 import { Link } from 'react-router-dom';
 
 
 export default function Reviews() {
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const API_URL = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const fetchRecentReviews = async () => {
+      try {
+        const response = await fetch(`${API_URL}/reviews`);
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log(data)
+        setReviews(data)
+      } catch (err) {
+        console.error('Fetch error:', err);
+        setError(err.message);
+      } finally {
+        setLoading(false)
+      }
+    };
+    fetchRecentReviews();
+  }, [API_URL]);
+
+  if (loading) return <div>Loading recent reviews...</div>
+  if (error) return <div>Error: { error}</div>
   return (
     <>
       <div className='nav-bar'>
@@ -19,9 +47,25 @@ export default function Reviews() {
       <p className="reviews-intro">
         Read honest employee experiences from companies across different industries
       </p>
-
-      
       </div>
+      <div className="recent-reviews">
+      <h2>Recent Reviews (Last 3 Days)</h2>
+      <div className="reviews-list">
+  {reviews.map((review, index) => (
+    <div key={index} className="review-card">
+      <div className="review-header">
+        <h3>{review.companyName}</h3>
+        <span className="review-date">{review.date}</span>
+      </div>
+      <p><strong>Pros:</strong> {review.pros}</p>
+      <p><strong>Cons:</strong> {review.cons}</p>
+      <p><strong>Advice:</strong> {review.advice}</p>
+      <p><strong>Department:</strong> {review.department} — <strong>Title:</strong> {review.jobTitle}</p>
+    </div>
+  ))}
+</div>
+
+    </div>
       </>
   );
 }
--- corpRate-backend/getRecentReviews/app.py
+++ corpRate-backend/getRecentReviews/app.py
@@ -2,18 +2,26 @@ import boto3
 import os
 from datetime import datetime,timedelta,timezone
 import json
+from boto3.dynamodb.types import TypeDeserializer
+from decimal import Decimal
 
 dynamodb = boto3.client('dynamodb')
 table_name = os.environ.get('TABLE_NAME')
 gsi_index = os.environ.get('GSI_NAME')
+deserializer = TypeDeserializer()
 
-
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError
+def deserialize_dynamodb_item(item):
+    return {k: deserializer.deserialize(v) for k, v in item.items()}
  
 
 def lambda_handler(event, context):
     # ISO stamp for 3 days ago?
     three_days_ago = (datetime.now(timezone.utc) - timedelta(days=3)).isoformat()
-    
+    print("Incoming event:",json.dumps(event))
     try:
         response = dynamodb.query(
         TableName = table_name,
@@ -26,13 +34,16 @@ def lambda_handler(event, context):
         ScanIndexForward=False
     )
         # parse the items from dynamodb json to flat dict
-        items = response['Items']
-        for item in items:
-            print({k: list(v.values())[0] for k, v in item.items()})
-           
+        items = [deserialize_dynamodb_item(item) for item in response['Items']]
+        print(items)   
         return {
-        'statusCode': 200,
-        'body': json.dumps(items)
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': 'http://localhost:5173',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            },
+            'body': json.dumps(items, default=decimal_default)
     }
     except Exception as e:
         return {
