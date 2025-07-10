d453af01dd50107474711347cca3b13a5a7e474e
ChristineMuchiri <tinniemuchiri@gmail.com>
2025-07-11 02:56:51 +0300

    cognito user pools and user pool client

--- company-review-front/src/aws-exports.js
+++ company-review-front/src/aws-exports.js
@@ -1,8 +1,15 @@
 export const awsConfig = {
-    Auth: {
-        region: import.meta.env.VITE_AWS_REGION,
-        userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
-        userPoolWebClientId: import.meta.env.VITE_AWS_USER_POOL_WEB_CLIENT_ID,
-    }
+  Auth: {
+    region: import.meta.env.VITE_AWS_REGION,
+    userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.VITE_AWS_USER_POOL_WEB_CLIENT_ID,
+  },
+  oauth: {
+    domain: import.meta.env.VITE_AWS_COGNITO_DOMAIN,
+    scope: ['openid', 'email', 'profile'],
+    redirectSignIn: import.meta.env.VITE_SIGNIN_REDIRECT_URL,
+    redirectSignOut: import.meta.env.VITE_SIGNOUT_REDIRECT_URL,
+    responseType: 'code', // or 'token'
+  }
 };
-console.log("AWS Config:", awsConfig.Auth);
+console.log('Resolved Amplify Config:', awsConfig);
